f1b680451aeb5e2d2e958af905933fd0b10e70af
ravi3222 <kashyap.ravi3222@gmail.com>
2021-01-15 05:07:00 +0530

    Update Product cards with redux state / Redux Hooks

--- src/containers/ProductListPage/index.js
+++ src/containers/ProductListPage/index.js
@@ -1,11 +1,21 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Layout from "../../components/Layout";
 import { getProductsBySlug } from "../../redux/actions";
+import { generatePublicUrl } from "../../urlConfig";
 import "./styles.scss";
 
 function ProductListPage(props) {
   const dispatch = useDispatch();
+  const [priceRange, setPriceRange] = useState({
+    under5k: 5000,
+    under10k: 10000,
+    under15k: 15000,
+    under20k: 20000,
+    under25k: 25000,
+    under30k: 30000,
+  });
+  const product = useSelector((state) => state.product);
 
   useEffect(() => {
     const { slug } = props.match.params;
@@ -14,46 +24,38 @@ function ProductListPage(props) {
   }, []);
   return (
     <Layout>
-      <div className="card">
-        <div className="cardHeader">
-          <div>Samsung Mobile Under 10k</div>
-          <button>View All</button>
-        </div>
-        <div>
-          <div className="productContainer">
-            <div className="productImgContainer">
-              <img
-                src="http://localhost:5000/public/VycvaFMd-W-galaxy-m31-sm-m315f-ds-samsung-original-imafyz8xgpzkzdqz.jpeg"
-                alt=""
-              />
-            </div>
-            <div className="productInfo">
-              <div>Samsung Galaxy 4gb phone</div>
+      {Object.keys(product.productsByPrice).map((key, index) => {
+        return (
+          <div className="card">
+            <div className="cardHeader">
               <div>
-                <span>4.3</span>
-                <span>3353</span>
+                {props.match.params.slug} Mobiles under {priceRange[key]}
               </div>
-              <div className="productPrice">5000</div>
+              <button>View All</button>
+            </div>
+            <div style={{ display: "flex" }}>
+              {product.productsByPrice[key].map((product) => (
+                <div className="productContainer">
+                  <div className="productImgContainer">
+                    <img
+                      src={generatePublicUrl(product.productPictures[1].img)}
+                      alt=""
+                    />
+                  </div>
+                  <div className="productInfo">
+                    <div style={{ margin: "5px 0" }}>{product.name}</div>
+                    <div>
+                      <span>4.3</span>
+                      <span>3353</span>
+                    </div>
+                    <div className="productPrice">{product.price}</div>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
-          <div className="productContainer">
-            <div className="productImgContainer">
-              <img
-                src="http://localhost:5000/public/VycvaFMd-W-galaxy-m31-sm-m315f-ds-samsung-original-imafyz8xgpzkzdqz.jpeg"
-                alt=""
-              />
-            </div>
-            <div className="productInfo">
-              <div>Samsung Galaxy 4gb phone</div>
-              <div>
-                <span>4.3</span>&nbsp;
-                <span>3353</span>
-              </div>
-              <div className="productPrice">5000</div>
-            </div>
-          </div>
-        </div>
-      </div>
+        );
+      })}
     </Layout>
   );
 }
