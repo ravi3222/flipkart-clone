a256d63a16ed0d336ecc3f145b3e20a97a726aeb
ravi3222 <kashyap.ravi3222@gmail.com>
2021-03-09 22:23:19 +0530

    getProductPage redux action

--- src/redux/actions/product.actions.js
+++ src/redux/actions/product.actions.js
@@ -12,3 +12,33 @@ export const getProductsBySlug = (slug) => {
     }
   };
 };
+
+export const getProductPage = (payload) => {
+  return async (dispatch) => {
+    try {
+      const { cid, type } = payload.params;
+      console.log(payload);
+      const res = await axios.get(`/page/${cid}/${type}`);
+      console.log("getProductPage", res);
+      dispatch({
+        type: productConstants.GET_PRODUCT_PAGE_REQUEST,
+      });
+
+      if (res.status === 200) {
+        const { page } = res.data;
+        dispatch({
+          type: productConstants.GET_PRODUCT_PAGE_SUCCESS,
+          payload: { page },
+        });
+      } else {
+        const { error } = res.data;
+        dispatch({
+          type: productConstants.GET_PRODUCT_PAGE_FAILURE,
+          payload: { error },
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
