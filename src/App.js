63465f773de2e23e6337889e0c21c340713e6d61
ravi3222 <kashyap.ravi3222@gmail.com>
2021-04-02 17:11:47 +0530

    useEffect for isUserloggedIn check

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import "./default.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./containers/HomePage";
 import ProductListPage from "./containers/ProductListPage";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { isUserLoggedIn } from "./redux/actions";
+// import ProductDetailsPage from "./containers/ProductDetailsPage";
 
 function App() {
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!auth.authenticate) {
+      dispatch(isUserLoggedIn());
+    }
+  }, [auth.authenticate]);
+
   return (
     <div className="App">
       <Router>
