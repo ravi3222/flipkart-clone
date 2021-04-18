5c7a4f7aeb5872fea20f5a9e2da9faae7fbd096f
ravi3222 <kashyap.ravi3222@gmail.com>
2021-04-18 15:29:50 +0530

    Product details page

--- src/containers/ProductDetailsPage/index.js
+++ src/containers/ProductDetailsPage/index.js
@@ -1,7 +1,13 @@
 import React, { useEffect } from "react";
+import { IoIosArrowForward, IoIosStar, IoMdCart } from "react-icons/io";
 import { useDispatch, useSelector } from "react-redux";
 import Layout from "../../components/Layout";
+import { MaterialButton } from "../../components/MaterialUI";
 import { getProductDetailsById } from "../../redux/actions";
+import { generatePublicUrl } from "../../urlConfig";
+import { BiRupee } from "react-icons/bi";
+import { AiFillThunderbolt } from "react-icons/ai";
+import "./styles.scss";
 
 function ProductDetailsPage(props) {
   const dispatch = useDispatch();
@@ -17,9 +23,135 @@ function ProductDetailsPage(props) {
     dispatch(getProductDetailsById(payload));
   }, []);
 
+  if (Object.keys(product.productDetails).length === 0) {
+    return null;
+  }
+
   return (
     <Layout>
-      <div>{JSON.stringify(product)}</div>
+      <div className="productDescriptionContainer">
+        <div className="flexRow">
+          <div className="verticalImageStack">
+            {product.productDetails.productPictures.map((thumb, index) => (
+              <div className="thumbnail active">
+                <img src={generatePublicUrl(thumb.img)} alt={thumb.img} />
+              </div>
+            ))}
+          </div>
+          <div className="productDescContainer">
+            <div className="productDescImgContainer">
+              <img
+                src={generatePublicUrl(
+                  product.productDetails.productPictures[0].img
+                )}
+                alt={`${product.productDetails.productPictures[0].img}`}
+              />
+            </div>
+
+            {/* action buttons */}
+            <div className="flexRow">
+              <MaterialButton
+                title="ADD TO CART"
+                bgColor="#ff9f00"
+                textColor="#ffffff"
+                style={{
+                  marginRight: "5px",
+                }}
+                icon={<IoMdCart />}
+              />
+              <MaterialButton
+                title="BUY NOW"
+                bgColor="#fb641b"
+                textColor="#ffffff"
+                style={{
+                  marginLeft: "5px",
+                }}
+                icon={<AiFillThunderbolt />}
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          {/* home > category > subCategory > productName */}
+          <div className="breed">
+            <ul>
+              <li>
+                <a href="#">Home</a>
+                <IoIosArrowForward />
+              </li>
+              <li>
+                <a href="#">Mobiles</a>
+                <IoIosArrowForward />
+              </li>
+              <li>
+                <a href="#">Samsung</a>
+                <IoIosArrowForward />
+              </li>
+              <li>
+                <a href="#">{product.productDetails.name}</a>
+              </li>
+            </ul>
+          </div>
+          {/* product description */}
+          <div className="productDetails">
+            <p className="productTitle">{product.productDetails.name}</p>
+            <div>
+              <span className="ratingCount">
+                4.3 <IoIosStar />
+              </span>
+              <span className="ratingNumbersReviews">
+                72,234 Ratings & 8,140 Reviews
+              </span>
+            </div>
+            <div className="extraOffer">
+              Extra <BiRupee />
+              4500 off{" "}
+            </div>
+            <div className="flexRow priceContainer">
+              <span className="price">
+                <BiRupee />
+                {product.productDetails.price}
+              </span>
+              <span className="discount" style={{ margin: "0 10px" }}>
+                22% off
+              </span>
+              {/* <span>i</span> */}
+            </div>
+            <div>
+              <p
+                style={{
+                  color: "#212121",
+                  fontSize: "14px",
+                  fontWeight: "600",
+                }}
+              >
+                Available Offers
+              </p>
+              <p style={{ display: "flex" }}>
+                <span
+                  style={{
+                    width: "100px",
+                    fontSize: "12px",
+                    color: "#878787",
+                    fontWeight: "600",
+                    marginRight: "20px",
+                  }}
+                >
+                  Description
+                </span>
+                <span
+                  style={{
+                    fontSize: "12px",
+                    color: "#212121",
+                  }}
+                >
+                  {product.productDetails.description}
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
